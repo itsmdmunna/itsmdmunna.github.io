ea4c0df7dd2836b76e9d3370eb64c376547e8cac
Md Munna <149456162+itsmdmunna@users.noreply.github.com>
2026-03-18 16:53:49 +0530

    Delete assets/files/phishing-email-course directory

--- assets/files/phishing-email-course/assets/ogW4bFi2Yy-Gl954/story_content/6HjUT5vnOh2_captions.js
+++
@@ -1,1 +0,0 @@
-window.globalLoadJsAsset('story_content/6HjUT5vnOh2_captions.js', {"captions":[{"langCode":"en-US","data":"WEBVTT%0D%0A%0D%0ANOTE%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.112.36406.0%0D%0A%0D%0A00:00:00.075%20--%3E%2000:00:00.801%0D%0AOption%20A.%0D%0A%0D%0A"}]})

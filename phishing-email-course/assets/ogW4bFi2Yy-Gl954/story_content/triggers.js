b6428dd731fecc5d5e4560ea7476e876ecd3a182
Md Munna <149456162+itsmdmunna@users.noreply.github.com>
2026-03-18 17:01:51 +0530

    Add files via upload

---
+++ phishing-email-course/assets/ogW4bFi2Yy-Gl954/story_content/triggers.js
@@ -0,0 +1,76 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6SH245ANsie":
+        Script1();
+        break;
+      case "5t3RHWMit7N":
+        Script2();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+window.Script1 = function()
+{
+  player.once(() => {
+const target = object('5wNhvJtmGXy');
+const duration = 500;
+const easing = 'ease-out';
+const id = '6BEZaakICkD';
+const pulseAmount = 0.07;
+const delay = 5000;
+addToTimeline(
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+window.Script2 = function()
+{
+  const target = object('5tldGtROj8r');
+const duration = 500;
+const easing = 'ease-out';
+const id = '5slV0eftZLc';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};

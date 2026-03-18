da34030456d31f665c556d9cb376286ec0c6c181
Md Munna <149456162+itsmdmunna@users.noreply.github.com>
2026-03-18 20:30:53 +0530

    Add files via upload

---
+++ phishing-email-course/assets/2jRRvC7wutT6_44q/story_content/triggers.js
@@ -0,0 +1,50 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6Z8ujfARZY9":
+        Script1();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+window.Script1 = function()
+{
+  const target = object('5tldGtROj8r');
+const duration = 500;
+const easing = 'ease-out';
+const id = '5slV0eftZLc';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
